ab22dedf72622d54dd393aa7ad355b0a6b35d4e5
wobozkyng <42249669+wobozkyng@users.noreply.github.com>
2023-05-29 19:44:19 +0800

    fix(server/function): entity wipes

--- server/sv_function.lua
+++ server/sv_function.lua
@@ -3,39 +3,48 @@ function fetchEntities(data)
     if data.object then
         local objects = GetAllObjects()
         for i = 1, #objects do
-            if data.pId ~= -1 and NetworkGetFirstEntityOwner(objects[i]) ~= data.pId then
+            local entity = objects[i]
+            local netId = NetworkGetNetworkIdFromEntity(entity)
+            local firstOwner = Entities[netId] or NetworkGetFirstEntityOwner(entity)
+            if data.pId ~= -1 and firstOwner ~= data.pId then
                 goto nextobject
             end
-            if data.model ~= 0 and GetEntityModel(objects[i]) ~= data.model then
+            if data.model ~= 0 and GetEntityModel(entity) ~= data.model then
                 goto nextobject
             end
-            entities[#entities+1] = objects[i]
+            entities[#entities+1] = entity
             ::nextobject::
         end
     end
     if data.ped then
         local peds = GetAllPeds()
         for i = 1, #peds do
-            if data.pId ~= -1 and NetworkGetFirstEntityOwner(peds[i]) ~= data.pId then
+            local entity = peds[i]
+            local netId = NetworkGetNetworkIdFromEntity(entity)
+            local firstOwner = Entities[netId] or NetworkGetFirstEntityOwner(entity)
+            if data.pId ~= -1 and firstOwner ~= data.pId then
                 goto nextped
             end
-            if data.model ~= 0 and GetEntityModel(peds[i]) ~= data.model then
+            if data.model ~= 0 and GetEntityModel(entity) ~= data.model then
                 goto nextped
             end
-            entities[#entities+1] = peds[i]
+            entities[#entities+1] = entity
             ::nextped::
         end
     end
     if data.vehicle then
         local vehicles = GetAllVehicles()
         for i = 1, #vehicles do
-            if data.pId ~= -1 and NetworkGetFirstEntityOwner(vehicles[i]) ~= data.pId then
+            local entity = vehicles[i]
+            local netId = NetworkGetNetworkIdFromEntity(entity)
+            local firstOwner = Entities[netId] or NetworkGetFirstEntityOwner(entity)
+            if data.pId ~= -1 and firstOwner ~= data.pId then
                 goto nextvehicle
             end
-            if data.model ~= 0 and GetEntityModel(vehicles[i]) ~= data.model then
+            if data.model ~= 0 and GetEntityModel(entity) ~= data.model then
                 goto nextvehicle
             end
-            entities[#entities+1] = vehicles[i]
+            entities[#entities+1] = entity
             ::nextvehicle::
         end
     end
